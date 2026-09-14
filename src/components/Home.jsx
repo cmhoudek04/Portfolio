119583449dd352ce502e80f6ea4a132859ae0c46
Conner Houdek <cmhoudek04@gmail.com>
2026-09-14 15:29:19 -0500

    Removed text highlighting in home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -39,10 +39,9 @@ export default function Home() {
           animate="show"
         >
           <p className="font-mono text-lg text-white sm:text-2xl">
-            My name is <span className="text-yellow-200">Conner Houdek</span>. I am a senior in software engineering at Iowa State University who is <span className="text-yellow-200">passionate </span> 
-            about software and problem solving. I am experienced with several languages, frameworks, and tools  alongside a growing list of <span className="text-yellow-200">completed</span> and 
-            <span className="text-yellow-200"> varied</span> projects. I am seeking opportunities to utilize skills with <span className="text-yellow-200">web development</span>, 
-            <span className="text-yellow-200">embedded systems</span>, and <span className="text-yellow-200">app development</span>.
+            My name is Conner Houdek. I am a senior in software engineering at Iowa State University who is passionate about software and problem solving. 
+            I am experienced with several languages, frameworks, and tools alongside a growing list of completed and varied projects. 
+            I am seeking opportunities to utilize skills with web development, embedded systems, and app development.
           </p>
         </MotionDiv>
 
